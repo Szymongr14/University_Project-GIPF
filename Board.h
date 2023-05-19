fc08272c7e8c82e308e298a1b943e161c3ea6961
Szymongr14 <szymongr14@gmail.com>
2023-05-19 14:09:56 +0200

    add loading and printing board

--- Board.h
+++ Board.h
@@ -6,12 +6,19 @@
 #define GIPF_BOARD_H
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 
+struct HexCoords{
+    char x;
+    int y;
+};
+
 class BoardField{
-private:
+public:
     char sign;
-    bool isEmpty;
+    bool isEmpty,used = false;
+    HexCoords hex_coords;
 };
 
 
@@ -27,10 +34,80 @@ public:
 
     Board()= default;
 
-    void loadBoard(){
+    void loadBoard() {
+        int fixed_size = size-1;
+        int board_size = 2 * size - 1;
+
+        for (int r = 0; r < board_size; r++) {
+            int row_size = (2 * fixed_size + 1) - abs(fixed_size - r);
+
+            for (int q = 0; q < row_size; q++) {
+                cin >> Board_vector[r][q].sign;
+                Board_vector[r][q].used = true;
+
+                //handle coords
+                if(r<=size){
+                    Board_vector[r][q].hex_coords.x = 'a' + q;
+                    Board_vector[r][q].hex_coords.y = size - r +q;
+                }
+                else if(r>size){
+                    Board_vector[r][q].hex_coords.x = 'a' + q;
+                    Board_vector[r][q].hex_coords.y = size - r;
+                }
+
+                if (Board_vector[r][q].sign == '_') {
+                    Board_vector[r][q].isEmpty = true;
+                } else if (Board_vector[r][q].sign == 'W' || Board_vector[r][q].sign == 'B') {
+                    Board_vector[r][q].isEmpty = false;
+                }
+            }
+        }
     }
 
 
+    void printBoard() const {
+        int fixed_size = size-1;
+        int board_size = 2 * size - 1;
+        bool wasSpacing;
+
+        for (int r = 0; r < board_size; r++) {
+            int row_size = (2 * fixed_size + 1) - abs(fixed_size - r);
+            wasSpacing = false;
+            for (int q = 0; q < row_size; q++) {
+
+                if(!wasSpacing){
+                    for(int t=0;t<board_size-row_size;t++){
+                        cout<<" ";
+                    }
+                    wasSpacing = true;
+                }
+                cout << Board_vector[r][q].sign << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void printBoardWithCoords() const {
+        int fixed_size = size-1;
+        int board_size = 2 * size - 1;
+        bool wasSpacing;
+
+        for (int r = 0; r < board_size; r++) {
+            int row_size = (2 * fixed_size + 1) - abs(fixed_size - r);
+            wasSpacing = false;
+            for (int q = 0; q < row_size; q++) {
+
+                if(!wasSpacing){
+                    for(int t=0;t<board_size-row_size;t++){
+                        cout<<" ";
+                    }
+                    wasSpacing = true;
+                }
+                cout << Board_vector[r][q].hex_coords.x << Board_vector[r][q].hex_coords.y << " ";
+            }
+            cout << endl;
+        }
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -7,19 +7,23 @@ using namespace std;
 int main() {
     string command;
     GameState gameState;
-    Board board;
+    Board board(4);
     do{
         cout<<"Type command: ";
         cin>>command;
 
         if(command=="LOAD_GAME_BOARD"){
-            gameState.loadGameState();
-            gameState.printGameState();
+//            gameState.loadGameState();
+//            gameState.printGameState();
+
+            //cout<<"elo"<<endl;
             board.loadBoard();
 
         }
         else if(command=="PRINT_BOARD"){
-            cout<<"print"<<endl;
+            board.printBoard();
+            cout<<endl<<endl<<endl;
+            board.printBoardWithCoords();
         }
 
     }while(command!="QUIT" && command!="q");
